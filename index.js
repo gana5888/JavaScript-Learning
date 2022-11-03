44573416cd4ea1661556481b18a2efcf1a64b873
gana5888 <95035521+gana5888@users.noreply.github.com>
2022-11-03 20:45:13 +0530

    1 file modified

--- index.js
+++ index.js
@@ -25,3 +25,9 @@ myPromise.then(
   function(value) {myDisplayer(value);},
   function(error) {myDisplayer(error);}
 );
+
+setTimeout(myFunction, 3000);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "I love You !!";
+}
